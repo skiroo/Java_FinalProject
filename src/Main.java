aaef78aa7e581992f864bcf8c71fbb3ae8fdc32d
SKiro <122486591+skiroo@users.noreply.github.com>
2024-10-24 18:36:45 -0700

    Update Main.java
    Comments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 /**
- * Main class to start application
+ * The Main class serves as the entry point for the HomeEx application.
+ * It starts the application by launching the LogIn screen,
+ * which allows users to log in or sign up for an account.
  */
 public class Main {
     public static void main(String[] args) {
